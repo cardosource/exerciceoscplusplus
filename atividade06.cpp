d488f3b2e7f45732d4b79ecd896dd43ab90b6d34
cardoso <fernandes_punk_@hotmail.com>
2022-09-30 22:48:57 -0400

    Add files via upload

---
+++ atividade06.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float tamanho;
+    cout << "informe tamanho do  circulo : ";
+    cin >> tamanho;
+    cout << "Raio : " << tamanho/2 << "\n";
+
+
+    return 0;
+}
