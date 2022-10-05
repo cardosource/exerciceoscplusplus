31eb79990865788761b3fc44d6402e1e7e89e620
cardoso <fernandes_punk_@hotmail.com>
2022-10-04 21:59:53 -0400

    Add files via upload

---
+++ atividade07.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <math.h>  
+
+using namespace std;
+
+int main(){
+    float quadrado =0;
+    cout << "infome o tamanho do quadrado : ";
+    cin >> quadrado;
+    int resposta_quadrado = pow(quadrado,2);
+    cout << "Quadrado : " << resposta_quadrado << "\n" <<
+    "Dobro do quadrado : " << resposta_quadrado *2 << "\n";
+    return 0;
+}
