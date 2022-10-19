7ab842a7e881934d094b065d23850e08773e85fa
cardoso <fernandes_punk_@hotmail.com>
2022-10-19 19:08:11 -0400

    Add files via upload

---
+++ atividade09.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float fahrenheit;
+    cout<< "Informe temperatura Fahrenheit :";
+    cin >> fahrenheit;
+    float resultado =  (fahrenheit-32) * 5/9;
+    cout << fahrenheit << " °F  em" << " °C" <<
+    " é " << resultado;
+    return 0;
+}
