eace12f3527981f7be5a3ebd8610248eccccd5e4
cardoso <jefersoncardoso.dev@gmail.com>
2022-11-09 02:20:21 -0400

    2 - Faça um Programa que peça um número e então mostre a mensagem ''O número informado foi [número]''.

--- atividade02.cpp
+++ atividade02.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 int main(){
+    
     int decimal;
     std::cout << "Digite um numero decimal : ";
     std::cin >> decimal;
     std::cout << "Numero decimal inteiro digitado : " << decimal << "\n";
+    
     return 0;
 }
